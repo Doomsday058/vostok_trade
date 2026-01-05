030032ffa1f631d2cbdd6cf8b9655f62bccc5c88
Your Name <your.email@example.com>
2026-01-06 01:17:42 +0300

    screen

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
+    // Игнорируем ошибки линтера при сборке (чтобы не падал на unused vars)
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Игнорируем ошибки TypeScript при сборке (чтобы не падал на any)
     ignoreBuildErrors: true,
   },
 };
